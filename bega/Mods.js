0ea27f1b8fa8e103a1b5078b2d97919cfff3c5c0
Barakabegaaa <dapsbegaaa@gmail.com>
2024-08-24 18:31:30 +0300

    Update Mods.js

--- bega/Mods.js
+++ bega/Mods.js
@@ -487,7 +487,7 @@ zokou({ nomCom: "save", categorie: "Mods" }, async (dest, zk, commandeOptions) =
           let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.stickerMessage)
   
           let stickerMess = new Sticker(media, {
-            pack: 'BARAKA-MD-V2-TAG',
+            pack: 'BARAKA-MD-V1-TAG',
             type: StickerTypes.CROPPED,
             categories: ["🤩", "🎉"],
             id: "12345",
